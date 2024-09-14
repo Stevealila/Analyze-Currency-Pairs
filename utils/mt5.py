172282d578e3e6e7ff79f95a2e5277565c5929e4
Stevealila <stevealila25@gmail.com>
2024-09-14 14:45:12 +0300

    Predict with SARIMAX and RNN (LSTM) models

--- utils/mt5.py
+++ utils/mt5.py
@@ -1,5 +1,12 @@
-def mt5_login():
 
+def initialize_():
+    import MetaTrader5 as mt5
+    mt5.initialize()
+
+
+
+
+def login_():
     import MetaTrader5 as mt5
     from dotenv import load_dotenv 
     import os
@@ -18,22 +25,21 @@ def mt5_login():
 
 
 
-def create_dataframe(currency_pair, timeframe, years_back=3):
-
+def get_currency_pair_data_(currency_pair, timeframe='D1', years_back=3):
     import MetaTrader5 as mt5
     import pytz
     from datetime import datetime
     import pandas as pd
 
     timeframe_map = {
-        "D1": mt5.TIMEFRAME_D1,
+        "M30": mt5.TIMEFRAME_M30,
         "H1": mt5.TIMEFRAME_H1,
         "H3": mt5.TIMEFRAME_H3,
         "H6": mt5.TIMEFRAME_H6,
         "H12": mt5.TIMEFRAME_H12,
-        "M30": mt5.TIMEFRAME_M30,
+        "D1": mt5.TIMEFRAME_D1,
         "W1": mt5.TIMEFRAME_W1,
-        "MN1": mt5.TIMEFRAME_MN1,
+        "M": mt5.TIMEFRAME_MN1,
     }
 
     if timeframe.upper() not in timeframe_map:
@@ -41,24 +47,22 @@ def create_dataframe(currency_pair, timeframe, years_back=3):
     
     # Calculate the number of bars based on timeframe and years_back
     mt5_timeframe = timeframe_map[timeframe.upper()]
+    if mt5_timeframe == mt5.TIMEFRAME_M30:  # Every 30 minutes
+        bars = years_back * 365 * 24 * 60 // 30
+    if mt5_timeframe == mt5.TIMEFRAME_H1:  # Hourly
+        bars = years_back * 365 * 24
+    if mt5_timeframe == mt5.TIMEFRAME_H3:  # Every 3 hours
+        bars = years_back * 365 * 24 // 3
+    if mt5_timeframe == mt5.TIMEFRAME_H6:  # Every 6 hours
+        bars = years_back * 365 * 24 // 6
+    if mt5_timeframe == mt5.TIMEFRAME_H12:  # Every 12 hours
+        bars = years_back * 365 * 24 // 12
     if mt5_timeframe == mt5.TIMEFRAME_D1:  # Daily
         bars = years_back * 365 
-    elif mt5_timeframe == mt5.TIMEFRAME_H1:  # Hourly
-        bars = years_back * 365 * 24
-    elif mt5_timeframe == mt5.TIMEFRAME_H3:  # Every 3 hours
-        bars = years_back * 365 * 24 // 3
-    elif mt5_timeframe == mt5.TIMEFRAME_H6:  # Every 6 hours
-        bars = years_back * 365 * 24 // 6
-    elif mt5_timeframe == mt5.TIMEFRAME_H12:  # Every 12 hours
-        bars = years_back * 365 * 24 // 12
-    elif mt5_timeframe == mt5.TIMEFRAME_M30:  # Every 30 minutes
-        bars = years_back * 365 * 24 * 60 // 30
-    elif mt5_timeframe == mt5.TIMEFRAME_W1:  # Weekly
+    if mt5_timeframe == mt5.TIMEFRAME_W1:  # Weekly
         bars = years_back * 52  
-    elif mt5_timeframe == mt5.TIMEFRAME_MN1:  # Monthly
+    if mt5_timeframe == mt5.TIMEFRAME_MN1:  # Monthly
         bars = years_back * 12  
-    else:
-        bars = years_back * 365  # Default to daily bars
 
     rates = mt5.copy_rates_from(
         currency_pair.upper()+"m", 
@@ -69,6 +73,7 @@ def create_dataframe(currency_pair, timeframe, years_back=3):
     if rates is not None and len(rates):  
         df = pd.DataFrame(rates)
         df['time'] = pd.to_datetime(df['time'], unit='s')
+        df = df.set_index('time')
         df.drop('real_volume', axis=1, inplace=True)
         return df
     else:
